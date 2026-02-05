7e43028bb4d9ad6a89399fb40f684a535e29ae56
FruKun <peromata@yandex.ru>
2026-02-05 12:55:01 +0500

    init

---
+++ core/bot_commands.py
@@ -0,0 +1,14 @@
+def responce_start():
+    return
+
+
+def responce_info():
+    return
+
+
+def responce_power_on():
+    return
+
+
+def responce_power_off():
+    return
---
+++ core/init.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+VK_TOKEN = os.getenv("VK_TOKEN")
+YANDEX_TOKEN = os.getenv("YANDEX_TOKEN")
